ee56bff0f82adc089429ecaddbe6236284c32107
ecandotti <enzo.candotti16@hotmail.com>
2020-12-07 02:04:29 +0100

    adapt for the project

--- index.js
+++ index.js
@@ -4,58 +4,64 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const app = express()
-const ContactRoutes = express.Router()
+const SongRoutes = express.Router()
 
-const PORT = process.env.PORT || 3000
-let Contact = require('./contact.model')
+const PORT = 8181
+let Song = require('./song.model')
 
 app.use(bodyParser.json())
 app.use(cors())
-mongoose.connect('mongodb+srv://pizzAdmin:BAISETAMER69@cluster0.eifrg.mongodb.net/PizzAPPdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/LucioDB', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const connection = mongoose.connection
 connection.once('open', function() {
     console.log("Connexion à la base de donnée MongoDB réussie")
 })
 
-/*          ROUTE CONTACT         */
-ContactRoutes.route('/list')
+/*          ROUTE SONG         */
+SongRoutes.route('/list')
     .get((req, res) => {
-        Contact.find(function(err, list_res) {
+        Song.find(function(err, list_song) {
             if (err) {
                 console.log(err)
             } else {
-                res.json(list_res)
+                res.json(list_song)
             }
         })
     })
 
-ContactRoutes.route('/:_id')
+SongRoutes.route('/byID/:songName')
     .get((req, res) => {
-        Contact.findById(req.params._id, (err, contact) => {
+        console.log(req.params.songName)
+        Song.find({songName: req.params.songName}, (err, song) => {
             if(err){
+                console.log(err)
                 res.send(err)
+            } else if(song.length == 0) {
+                console.log("No record found")
+                return
             }
-            res.json(contact)
+            console.log(song)
+            res.json(song)
         })
     })
 
-ContactRoutes.route('/add')
+SongRoutes.route('/add')
     .post((req, res) => {
-        let contact = new Contact(req.body)
-        console.log(contact)
-        contact.save()
-            .then(contact => {
-                res.status(200).json({'contact': 'added successfully'})
+        let song = new Song(req.body)
+        console.log(song)
+        song.save()
+            .then(song => {
+                res.status(200).json({'song': 'added successfully'})
                 console.log('saved')
             })
             .catch(err => {
-                res.status(400).json({'contact': err})
+                res.status(400).json({'song': err})
                 console.log('no saved')
             })
     })
 
-app.use('/contacts', ContactRoutes)
+app.use('/songs', SongRoutes)
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT)
